4d00996a80794e9b9c91dec3d03cc74e0c408a9b
sanghuniolsida <p0526000@gmail.com>
2025-09-04 00:42:16 +0900

    style: 채팅방 스타일 수정

--- src/Pages/ChatSetting/ChatSetting.tsx
+++ src/Pages/ChatSetting/ChatSetting.tsx
@@ -44,10 +44,10 @@ type Draft = {
 };
 type LocationState =
   | {
-      selectedUserNoteDescription?: string | null;
-      draft?: Draft;
-      fromSearch?: string;
-    }
+    selectedUserNoteDescription?: string | null;
+    draft?: Draft;
+    fromSearch?: string;
+  }
   | null;
 
 const ChatSetting: React.FC = () => {
@@ -84,7 +84,7 @@ const ChatSetting: React.FC = () => {
           setCharacterName(data.result.name ?? '');
           setCharacterImageUrl(data.result.characterImageUrl ?? '');
         }
-      } catch {}
+      } catch { }
     })();
   }, [characterId]);
 
@@ -151,7 +151,7 @@ const ChatSetting: React.FC = () => {
       setName(persona.name || '');
       setGender(mapGender(persona.gender));
       setIntroduction(persona.persona || '');
-    } catch {}
+    } catch { }
   };
 
   const handleOpenUserNotes = useCallback(async () => {
